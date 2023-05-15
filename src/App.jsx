738aa2e88cacc72c4dfd56fa389732093c3185f7
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-05-15 18:40:35 +0200

    feat: added base for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/header/Header'
+import { Nav } from './components/nav/Nav'
+import { About } from './components/about/About'
+import { Experience } from './components/experience/Experience'
+import { Services } from './components/services/Services'
+import { Contact } from './components/contact/Contact'
+import { Portfolio } from './components/portfolio/Portfolio'
+import { Footer } from './components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      
-      </div>
-   
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Experience.css'
+export const Experience = () => {
+  return (
+    <div>Experience</div>
+  )
+}
